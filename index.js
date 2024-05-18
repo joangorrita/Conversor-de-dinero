80a44019707033190f1d6156bd8fcfeb6044fb07
joan <joangorrita01@gmail.com>
2024-05-18 17:18:34 -0300

    Se arregla el bucle while y se termina

--- index.js
+++ index.js
@@ -5,27 +5,36 @@ let resultado;
 
 switch (tipoMoneda) {
     case 1: 
-        resultado = moneda * 0.0011;
+        resultado = Number(moneda) * 0.0011;
         console.log(resultado);
         break;
     case 2: 
-        resultado = moneda * 0.0010;
+        resultado = Number(moneda) * 0.0010;
         console.log(resultado);
         break;
     case 3:
-        resultado = moneda * 0.0009;
+        resultado = Number(moneda) * 0.0009;
         console.log(resultado);
         break;
     default:
-        let i = 0;
-        while (i = 1 ){
-            if (isNaN(tipoMoneda) || tipoMoneda < 1 || tipoMoneda > 3 ) {
-                console.log("por favor ingrese una de las opciones indicadas  1.USD   2.EUR   3.COP");
-            }
+        while (isNaN(tipoMoneda) || tipoMoneda < 1 || tipoMoneda > 3) {
+            console.log("Por favor ingrese una de las opciones indicadas 1.USD   2.EUR   3.GBP");
+            tipoMoneda = parseInt(prompt("Indique el tipo de moneda 1.USD   2.EUR   3.GBP "));
+        }
+        // ingreso una moneda valida y deberia salir del bucle y hacer lo mismo 
+        switch (tipoMoneda) {
+            case 1: 
+                resultado = Number(moneda) * 0.0011;
+                console.log(resultado);
+                break;
+            case 2: 
+                resultado = Number(moneda) * 0.0010;
+                console.log(resultado);
+                break;
+            case 3:
+                resultado = Number(moneda) * 0.0009;
+                console.log(resultado);
+                break;
         }
         break;
 }
-
-
-// mirar los ultimos if
-
